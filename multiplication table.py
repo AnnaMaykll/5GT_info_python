6d0edff5f4b9751a42875676d033966f7d42045c
AnnaMaykll <koullaannamay@outlook.be>
2025-03-28 12:13:41 +0100

    Create multiplication table.py

---
+++ multiplication table.py
@@ -0,0 +1,6 @@
+nombre=input("Bonjour,bienvnue dans ce programme de multiplication. De quel nombre voulez vous la table de multiplication ?")
+
+nombre=int(nombre)
+
+for i in range(1,11000):
+  print(i,"x",nombre,"=",i*nombre)
