dfc89ffd5e8997c814d77aa7f4668c75dc19d7cf
AnnaMaykll <koullaannamay@outlook.be>
2025-05-23 12:25:25 +0200

    Create pendu.py

---
+++ pendu.py
@@ -0,0 +1,111 @@
+#Hanna-Lynn et Anna-May
+
+print("Bonjour et bienvenue dans ce programme qui joue au pendu (ecrivez tout en minuscule).")
+print("Le mot contient 6 lettres que vous devez trouvez.")
+
+#1
+lettre=input("Quelle lettre proposez-vous?")
+
+mot="crayon"
+
+if lettre in mot:
+    print("C'est une bonne lettre.")
+    print()
+    
+elif lettre not in mot :
+    print("Ce n'est pas la bonne lettre.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+    
+    
+#2
+lettre=input("Quelle lettre proposez-vous?")
+
+mot="crayon"
+
+if lettre in mot:
+    print("C'est une bonne lettre.")
+    print()
+    
+elif lettre not in mot :
+    print("Ce n'est pas la bonne lettre.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+    
+    
+    
+#3
+lettre=input("Quelle lettre proposez-vous?")
+
+mot="crayon"
+
+if lettre in mot:
+    print("C'est une bonne lettre.")
+    print()
+    
+elif lettre not in mot :
+    print("Ce n'est pas la bonne lettre.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+    
+#4
+lettre=input("Quelle lettre proposez-vous?")
+
+mot="crayon"
+
+if lettre in mot:
+    print("C'est une bonne lettre.")
+    print()
+    
+elif lettre not in mot :
+    print("Ce n'est pas la bonne lettre.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+    
+#5
+lettre=input("Quelle lettre proposez-vous?")
+
+mot="crayon"
+
+if lettre in mot:
+    print("C'est une bonne lettre.")
+    print()
+    
+elif lettre not in mot :
+    print("Ce n'est pas la bonne lettre.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+
+
+reponse=input("Avez-vous devinez le mot?")
+print()
+
+if reponse=="crayon" :
+    print("Bien Jouer tu as gagné!!")
+    print()
+    
+elif reponse!="crayon" :
+    print("C'est faux, dommage.")
+    print()
+    
+else :
+    print("Il y a un problème.")
+    print()
+
+    
+
